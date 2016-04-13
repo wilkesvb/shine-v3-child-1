d0e2367ac20693aada6a9779d3762eccdf3753df
wilkesvb <vincentwilkes@MacBook-Pro.local>
2016-04-13 15:16:48 -0400

    Media query, some dynamic, right column styles
    Still need to add in newsletter section and then add in actual content.

--- emp-portal-home.php
+++ emp-portal-home.php
@@ -79,15 +79,45 @@
 					</section>
 
 					<section>
+
 						<h4>APP Downloads</h4>
-						<ul>
-							<li><img src="" alt="">
-									<p>Deltek GovCon App</p>
-								</img></li>
-							<li><img src="" alt="">
-									<p>Insperity App</p>
-								</img></li>
-						</ul>
+
+							<?php 
+
+								$args = array(
+									'post_type' => 'emp-apps'
+								);
+								 $query = new WP_Query( $args );
+							?>
+
+							<?php if( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
+
+								<div>
+
+									<?php 
+
+									$image = get_field('app_logo');
+
+									if( !empty($image) ): ?>
+
+									<div class="col-1-2"><img class="app-logo" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" /></div>
+
+									<?php endif; ?>
+
+									<div class="col-1-2"><h5><?php the_field( 'app_name' ) ?></h5></div>
+
+									<div class="clear"></div>
+
+									<a href="<?php the_field( 'app_store_link' ) ?>" target="_blank" ><img class="store-button" src="/wp-content/uploads/2016/04/App_Store_icon-1-copy.png" alt="Available on the App Store" /></a>
+
+									<a href="<?php the_field( 'google_play_link' ) ?>" target="_blank" ><img class="store-button" src="/wp-content/uploads/2016/04/google-store-badge.png" alt="Available on Google Play" /></a>
+
+									<div class="clear"></div>
+
+								</div>
+
+							<?php endwhile; endif; wp_reset_postdata(); ?>
+
 					</section>
 
 				</div>
