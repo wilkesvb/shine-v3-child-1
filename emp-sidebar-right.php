7834842b7e47c08b337ad1614b558248b4f3c37f
wilkesvb <vincentwilkes@Mac-3c15c2cedbac.local>
2016-04-19 14:33:40 -0400

    Logic, styles, etc
    Added User CP section, added php logic for loading CSS and redirecting
    pages after log in and during session. Still need to redirect
    /employees unlogged to log in

--- emp-sidebar-right.php
+++ emp-sidebar-right.php
@@ -42,4 +42,19 @@
 
 	</section>
 
+	<section>
+
+		<h4>User CP</h4>
+
+		<div>
+
+			<ul>
+				<li><a href="/membership-login/password-reset/">Password Reset</a></li>
+				<li><a href="/membership-login/?swpm-logout=true">Log Out</a></li>
+			</ul>
+
+		</div>
+
+	</section>
+
 </div>
--- functions.php
+++ functions.php
@@ -1,7 +1,30 @@
-<?php function child_theme_styles() {  /*wpt is for name spacing, unique so it doesn't conflict with other functions or theme calls*/
+<?php 
 
-	wp_enqueue_style( 'emp-portal_css', get_stylesheet_directory_uri() . '/stylesheets/emp-portal.css' );
+function child_theme_styles() {  /*wpt is for name spacing, unique so it doesn't conflict with other functions or theme calls*/
+	
+	$emppages = array( 'employees', 'membership-login', 'password-reset');
+
+	if ( is_page( $emppages ) ) {
+		wp_enqueue_style( 'emp-portal_css', get_stylesheet_directory_uri() . '/stylesheets/emp-portal.css' );
+	}
 }
+
 add_action( 'wp_enqueue_scripts', 'child_theme_styles' );
+
+// function redirect_unlogged_in_user() {
+// 	if ( is_user_logged_in( 'false' ) && is_page( 'employees' ) ) {
+// 	wp_redirect('/membership-login/');
+// 	exit;
+// 	}
+// }
+// add_action( 'template_redirect', 'redirect_unlogged_in_user' );
+
+function redirect_logged_in_user() {
+	if ( is_user_logged_in() && is_page( 'portal' ) ) {
+	wp_redirect('/employees/');
+	exit;
+	}
+}
+add_action( 'template_redirect', 'redirect_logged_in_user' );
 
 ?>
