9deda5633afc113f44aef5d2bac6335a2b5b3613
wilkesvb <vincentwilkes@MacBook-Pro.local>
2016-05-16 13:10:17 -0400

    suggestion box

--- emp-suggestion-box.php
+++ emp-suggestion-box.php
@@ -1,9 +1,9 @@
-<div>
+<div class="suggestion-box">
 
-	<a href="mailto:suggestions@shinesystech.com" style="margin: 0 auto; width:50px;"></a>
+	<a href="mailto:suggestions@shinesystech.com" ><img src="/wp-content/uploads/2016/05/suggestion-box-red.png" alt="make a suggestion"/></a>
 
 
 	<p style="font-size: 10px">*clicking will launch email application</p>
 
-	<a href="https://shinesystech.sharepoint.com/usershares/company/_layouts/15/DocIdRedir.aspx?ID=74469-45-4841">click</a>
+
 </div>
