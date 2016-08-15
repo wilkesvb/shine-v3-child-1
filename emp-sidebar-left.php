f5878a18ca516d142d07a3465f6c6227b4985083
wilkesvb <vincentwilkes@MacBook-Pro.local>
2016-08-15 11:39:42 -0400

    Stripped Out Employee Portal

--- emp-sidebar-left.php
+++
@@ -1,7 +0,0 @@
-<div class="emp-sidebar">
-	
-	<?php get_template_part( 'hot-jobs' ); ?>
-
-	<?php get_template_part( 'emp-documents' ); ?>
-
-</div>
--- emp-suggestion-box.php
+++
@@ -1,9 +0,0 @@
-<div class="suggestion-box">
-
-	<a href="mailto:suggestions@shinesystech.com" ><img src="/wp-content/uploads/2016/05/suggestion-box-red.png" alt="make a suggestion"/></a>
-
-
-	<p style="font-size: 10px">*clicking will launch email message</p>
-
-
-</div>
